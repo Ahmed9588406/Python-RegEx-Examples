57be54307eff57b3d69f5823fd85ec0cce80a3ce
Ahmed9588406 <ak9588406@gmail.com>
2024-07-06 20:48:20 +0300

    initial commit

---
+++ text_string.py
@@ -0,0 +1,51 @@
+test_string = '123abc456789abc123ABC'
+
+test_string = 'python-engineer.com'
+
+test_string = 'hello 123_ heyho hohey'
+
+test_string = 'hello 123_'
+
+dates = """
+01.04.2020
+
+2020.04.01
+
+2020-04-01
+
+2020-05-23
+
+2020-06-11
+
+2020-07-11
+
+2020-08-11
+
+2020/04/04
+
+2020_04_04
+
+2020_04_04
+
+"""
+
+
+my_string = """
+Mr Simpson
+Mrs Simpson
+Mr. Brown
+Ms Smith
+Mr.T
+"""
+
+emails = """
+pythonengineer@gmail.com
+Python-engineer@gmx.de
+python-engineer123@my-domain.org
+"""
+
+urls = """
+http://python-engineer.com
+https://www.python-engineer.com
+http://www.pyeng.net
+"""
